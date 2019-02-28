1c121f0f985f06d1b21b8730fc75c24850b1d279
jessechen1618 <jchen03@stuy.edu>
2019-02-28 13:52:43 -0500

    First Modification

--- FirstGit.java
+++ FirstGit.java
@@ -1,5 +1,6 @@
 public class FirstGit {
 	public static void main(String[] args) {
 		System.out.println("Hello world! Hello Git!");
+		System.out.println("First modification. Ready to push.");
 	}
 }
